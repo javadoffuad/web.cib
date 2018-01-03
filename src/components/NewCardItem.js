17a3a9da5ac17fdf6323a19723bd53c0c64afb80
javadoffuad <javadoffuad@gmail.com>
2018-01-03 21:49:21 +0300

    new card - color picker

---
+++ src/components/NewCardItem.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default (props) => {
+    return(
+        <div className="pane newcard-sprite">
+            <div className="pane newcard card-front" id="cardfront" style={{backgroundColor: "rgb(138, 190, 107)", margin: "0px 0px 0px 55px"}}>
+                <div className="pane input-sprite multiple">
+                    <div className="input-block">
+                        <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
+                        <input type="tel" placeholder="0000" maxLength="4" id="pan1" data-next="pan2" tabIndex="1" autoComplete="off" />
+                    </div>
+                    <div className="input-block">
+                        <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
+                        <input type="tel" placeholder="0000" maxLength="4" id="pan2" data-next="pan3" data-prev="pan1" tabIndex="2" autoComplete="off" />
+                    </div>
+                    <div className="input-block">
+                        <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
+                        <input type="tel" placeholder="0000" maxLength="4" id="pan3" data-next="pan4" data-prev="pan2" tabIndex="3" autoComplete="off" />
+                    </div>
+                    <div className="input-block">
+                        <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
+                        <input type="tel" placeholder="0000" maxLength="4" id="pan4" data-next="expiry" data-prev="pan3" tabIndex="4" autoComplete="off" />
+                    </div>
+                </div>
+                <div className="pane input-sprite right">
+                    <div className="input-block expry-block">
+                        <div className="tooltip" data-pos="top-left">Срок действия (10/12)</div>
+                        <input type="tel" placeholder="00/00" maxLength="5" id="expiry" data-next="cvv" data-prev="pan4" tabIndex="5" autoComplete="off" />
+                    </div>
+                </div>
+                <div className="pane input-sprite">
+                    <div className="input-block">
+                        <div className="tooltip" data-pos="top-left">Имя карты</div>
+                        <input className="name" type="text" placeholder="Имя карты" id="cardname" data-prev="cvv" tabIndex="7" autoComplete="off" />
+                    </div>
+                </div>
+            </div>
+            <div className="pane newcard card-back" id="cardback" style={{backgroundColor: "rgb(138, 190, 107)", left: "55px"}}>
+                <div className="pane magnetic-strip"></div>
+                <div className="pane input-sprite right">
+                    <div className="input-block cvv-block">
+                        <div className="tooltip" data-pos="top-left">Card cvv</div>
+                        <input type="tel" placeholder="CVV" maxLength="3" id="cvv" data-next="cardname" data-prev="expiry" tabIndex="6" autoComplete="off" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/containers/NewCard.js
+++ src/containers/NewCard.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { CirclePicker } from 'react-color'
 
 import Header from 'components/Header';
 import { createCard } from 'actions/cardActions';
+import NewCardItem from 'components/NewCardItem';
 
 class NewCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            card: {
+                type: null,
+                title: null,
+                color: null,
+                expiry: null,
+                number: null
+            }
+        }
+    }
     handleCard() {
         let card = {
             id: 5,
@@ -36,55 +51,16 @@ class NewCard extends Component {
                                             <p>Можно добавить желаемое число карт. <a href="https://cib.az#bl1-f">Подробная информация</a></p>
                                         </div>
                                     </div>
-                                    <div className="pane newcard-sprite">
-                                        <div className="pane newcard card-front" id="cardfront" style={{backgroundColor: "rgb(138, 190, 107)", margin: "0px 0px 0px 55px"}}>
-                                        <div className="pane input-sprite multiple">
-                                            <div className="input-block">
-                                                <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
-                                                <input type="tel" placeholder="0000" maxLength="4" id="pan1" data-next="pan2" tabIndex="1" autoComplete="off" />
-                                            </div>
-                                            <div className="input-block">
-                                                <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
-                                                <input type="tel" placeholder="0000" maxLength="4" id="pan2" data-next="pan3" data-prev="pan1" tabIndex="2" autoComplete="off" />
-                                            </div>
-                                            <div className="input-block">
-                                                <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
-                                                <input type="tel" placeholder="0000" maxLength="4" id="pan3" data-next="pan4" data-prev="pan2" tabIndex="3" autoComplete="off" />
-                                            </div>
-                                            <div className="input-block">
-                                                <div className="tooltip" data-pos="top-left">Номер карты (только цифры)</div>
-                                                <input type="tel" placeholder="0000" maxLength="4" id="pan4" data-next="expiry" data-prev="pan3" tabIndex="4" autoComplete="off" />
-                                            </div>
-                                        </div>
-                                        <div className="pane input-sprite right">
-                                            <div className="input-block expry-block">
-                                                <div className="tooltip" data-pos="top-left">Срок действия (10/12)</div>
-                                                <input type="tel" placeholder="00/00" maxLength="5" id="expiry" data-next="cvv" data-prev="pan4" tabIndex="5" autoComplete="off" />
-                                            </div>
-                                        </div>
-                                        <div className="pane input-sprite">
-                                            <div className="input-block">
-                                                <div className="tooltip" data-pos="top-left">Имя карты</div>
-                                                <input className="name" type="text" placeholder="Имя карты" id="cardname" data-prev="cvv" tabIndex="7" autoComplete="off" />
-                                            </div>
-                                        </div>
-                                        </div>
-                                        <div className="pane newcard card-back" id="cardback" style={{backgroundColor: "rgb(138, 190, 107)", left: "55px"}}>
-                                            <div className="pane magnetic-strip"></div>
-                                            <div className="pane input-sprite right">
-                                                <div className="input-block cvv-block">
-                                                    <div className="tooltip" data-pos="top-left">Card cvv</div>
-                                                    <input type="tel" placeholder="CVV" maxLength="3" id="cvv" data-next="cardname" data-prev="expiry" tabIndex="6" autoComplete="off" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <NewCardItem />
                                 </div>
+                                
                                 <div id="palette-block" className="pane newcard-info-block picker-width">
-                                    <div className="pane newcard-txt">
-                                        <h2 className="pane title">Цвет карты</h2>
+                                    <div class="pane newcard-txt">
+                                        <h2 class="pane title">Цвет карты</h2>
+                                        <div className="picker">
+                                            <CirclePicker />
+                                        </div>
                                     </div>
-    
                                     <div className="pane buttons">
                                         <div className="fields field-button">
                                             <div className="field">
