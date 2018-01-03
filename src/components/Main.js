b8012fbee32bf25a3c32ab0c1fd16c519c88e456
javadoffuad <javadoffuad@gmail.com>
2018-01-04 02:06:50 +0300

    route path for pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,12 +10,12 @@ import Settings from 'containers/Settings';
 export default () => {
     return (
         <Switch>
-            <Route exact path='/' component={Cards} />
-            <Route exact path='/cards' component={Cards} />
-            <Route path='/cards/add' component={NewCard} />
-            <Route path='/history' component={History}/>
-            <Route path='/template' component={Templates} />
-            <Route path='/settings' component={Settings}/>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Cards} />
+            <Route exact path={`${process.env.PUBLIC_URL}/cards`} component={Cards} />
+            <Route path={`${process.env.PUBLIC_URL}/cards/add`} component={NewCard} />
+            <Route path={`${process.env.PUBLIC_URL}/history`} component={History}/>
+            <Route path={`${process.env.PUBLIC_URL}/template`} component={Templates} />
+            <Route path={`${process.env.PUBLIC_URL}/settings`} component={Settings}/>
         </Switch>
     )
 }
